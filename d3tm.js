15e863d8756dc7584e141559103fccfec7a9b47f
timhagn <mail@timhagn.com>
2018-10-03 16:21:46 +0200

    finish d3tm

--- d3tm.js
+++ d3tm.js
@@ -1,163 +1,178 @@
-const projectName="tree-map"
+"use strict";
+const projectName = "tree-map";
 localStorage.setItem('example_project', 'D3: Tree Map');
 
 const rem2px = (rem) => (rem * parseFloat(getComputedStyle(document.documentElement).fontSize));
 
-const FILES = [
-    'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
-    'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
-];
-
-let container = document.getElementById('container'),
-    description = document.getElementById('description'),
-    margin = {
-      top: rem2px(1),
-      right: rem2px(1),
-      bottom: rem2px(4),
-      left: rem2px(1)
-    },
-    padding = {
-      top: rem2px(4.5),
-      right: rem2px(1),
-      bottom: rem2px(1),
-      left: rem2px(1),
-    },
-    width = container.offsetWidth
-        - margin.left - margin.right
-        - padding.left - padding.right,
-    height = container.offsetHeight
-        - margin.top - margin.bottom
-        - padding.top - padding.bottom;
-
-width = width < 960 ? 960 : width;
-
-const x = d3.scaleLinear()
-        .domain([2.6, 75.1])
-        .rangeRound([height, width]),
-      color = d3.scaleThreshold()
-        .domain(d3.range(2.6, 75.1, (75.1-2.6)/8))
-        .range(d3.schemeBlues[9]),
-      path = d3.geoPath();
-
-let chart = d3.select('.chart');
-
-let svgChart = chart.append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + padding.bottom);
-
-let tooltip = chart.append('div')
-    .attr('id', 'tooltip')
-    .style('opacity', 0);
-
-let legend = svgChart.append("g")
-    .attr("class", "key")
-    .attr("id", "legend")
-    .attr("transform", "translate(0,40)")
-
-legend.selectAll("rect")
-    .data(color.range().map((d) => {
-      d = color.invertExtent(d);
-      if (d[0] == null) d[0] = x.domain()[0];
-      if (d[1] == null) d[1] = x.domain()[1];
-      return d;
-    }))
-    .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", (d) => x(d[0]))
-    .attr("width", (d) => Math.abs(x(d[1]) - x(d[0])))
-    .attr("fill", (d) =>  color(d[0]));
-
-legend.append("text")
-    .attr("class", "caption")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
-    .attr("fill", "#000")
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .text("Educational Attainment");
-
-legend.call(d3.axisBottom(x)
-    .tickSize(13)
-    .tickFormat((x) => Math.round(x) + '%')
-    .tickValues(color.domain()))
-    .select(".domain")
-    .remove();
-
-// Create Chart promisified.
-Promise.all(FILES.map(url => d3.json(url)))
-    .then((data) => d3Process(data));
-
-const d3Process = (data) => {
-  // Fill US & Education Array from data.
-  const us = data[0].hasOwnProperty('type') ? data[0] : data[1];
-  const education = data[0].hasOwnProperty('type') ? data[1] : data[0];
-
-  // Add counties.
-  svgChart.append("g")
-      .attr("class", "counties")
-      .selectAll("path")
-      .data(topojson.feature(us, us.objects.counties).features)
-      .enter().append("path")
-      .attr("class", "county")
-      .attr("data-fips", (d) => d.id)
-      .attr("data-education", (d) => {
-        let result = education.filter(function( obj ) {
-          return obj.fips === d.id;
-        });
-        if(result[0]){
-          return result[0].bachelorsOrHigher
-        }
-        // Could not find a matching fips-id in the data.
-        console.log("Couldn't find data for: ", d.id);
-        return 0
-      })
-      .attr("fill", (d) => {
-        let result = education.filter(function( obj ) {
-          return obj.fips === d.id;
-        });
-        if(result[0]){
-          return color(result[0].bachelorsOrHigher)
-        }
-        // Could not find a matching color in the Data.
-        return color(0)
-      })
-      .attr("d", path)
-      .on("mouseover", (d) => {
-        let result = education.filter(function( obj ) {
-          return obj.fips === d.id;
-        });
-        if(result[0]) {
-          tooltip.style("opacity", .9);
-          tooltip.attr("data-education", result[0].bachelorsOrHigher);
-          tooltip.html(result[0].area_name
-              + ", " + result[0].state
-              + ": " + result[0].bachelorsOrHigher + "%");
-
-          // Calculate x and y positions for tooltip.
-          let mouseX = d3.event.pageX - svgChart.node().getBoundingClientRect().x + 10,
-              mouseY = d3.event.pageY - svgChart.node().getBoundingClientRect().y + 10,
-              y = mouseY + tooltip.node().offsetHeight + rem2px(1) > height ?
-                  mouseY - tooltip.node().offsetHeight / 2 + rem2px(1)
-                  : mouseY + tooltip.node().offsetHeight + rem2px(1),
-              x = mouseX - tooltip.node().offsetWidth / 2 > width ?
-                  mouseX - tooltip.node().offsetWidth + rem2px(1)
-                  : mouseX - tooltip.node().offsetWidth / 2 + rem2px(1);
-          if (x < 0) {
-            x = mouseX;
-          }
-          tooltip.style("left", (x) + "px")
-              .style("top", (y) + "px");
-        }
-      })
-      .on("mouseout", (d) => {
-        tooltip.style("opacity", 0);
-      });
-
-  // Add states.
-  svgChart.append("path")
-      .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-      .attr("class", "states")
-      .attr("d", path);
+const FILES = {
+  kickstarter: {
+    title: "Kickstarter Pledges",
+    url: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json"
+  },
+  movie: {
+    title: "Movie Sales",
+    url: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json"
+  },
+  game: {
+    title: "Video Game Sales",
+    url: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json"
+  }
 };
 
+const title = document.getElementById('title'),
+    description = document.getElementById('description'),
+    legendSize = {
+      offset: rem2px(0.5),
+      xOffset: 3,
+      yOffset: -2,
+      rectSize: rem2px(0.8),
+      hSpacing: rem2px(10),
+      vSpacing: rem2px(0.5),
+    };
 
+let svgChart = d3.select("#tree-map"),
+    width = +svgChart.attr("width"),
+    height = +svgChart.attr("height"),
+    body = d3.select('body'),
+    legend = d3.select("#legend"),
+    legendWidth = +legend.attr("width");
+
+let tooltip = body.append("div")
+    .attr("id", "tooltip")
+    .style("opacity", 0);
+
+// Processes the Chart.
+const d3Process = (data) => {
+  // Create new color-scale and treemap.
+  const color = d3.scaleOrdinal()
+          .range(d3.schemeCategory10
+              .map((c) => (d3.interpolateRgb(c, "#ccc")(0.1)))),
+        treemap = d3.treemap()
+          .size([width, height])
+          .paddingInner(1);
+
+  // Set Description from data.
+  description.innerHTML = data.name;
+
+  // Functions to show and hide tooltips both on tile-text and tiles.
+  const showTooltip = (d) => {
+    tooltip.style("opacity", .9)
+        .attr("data-value", d.data.value)
+        .style("left", (d3.event.pageX + rem2px(1)) + "px")
+        .style("top", (d3.event.pageY + rem2px(1)) + "px")
+        .html(
+            'Name: ' + d.data.name + '<br>'
+            + 'Category: ' + d.data.category + '<br>'
+            + 'Value: ' + d.data.value
+        );
+  };
+  const hideTooltip = (d) => {
+    tooltip.style("opacity", 0);
+  };
+
+  // Create Root tree.
+  let root = d3.hierarchy(data)
+      .eachBefore((d) => {
+        d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name;
+      })
+      .sum((d) => d.value)
+      .sort((a, b) => b.value - a.value || b.height - a.height);
+
+  // Create Treemap.
+  treemap(root);
+
+  // Create cells.
+  const cell = svgChart.selectAll("g")
+      .data(root.leaves())
+      .enter().append("g")
+      .attr("class", "group")
+      .attr("transform", (d) => "translate(" + d.x0 + "," + d.y0 + ")");
+
+  // Create tiles.
+  cell.append("rect")
+      .attr("id", (d) => d.data.id)
+      .attr("class", "tile")
+      .attr("width", (d) => d.x1 - d.x0)
+      .attr("height", (d) => d.y1 - d.y0)
+      .attr("data-name", (d) => d.data.name)
+      .attr("data-category", (d) => d.data.category)
+      .attr("data-value", (d) => d.data.value)
+      .attr("fill", (d) => color(d.data.category))
+      .on("mouseover", showTooltip)
+      .on("mouseout", hideTooltip);
+
+  // Add Tile Texts.
+  cell.append("text")
+      .attr('class', 'tile-text')
+      .selectAll("tspan")
+      .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+      .enter().append("tspan")
+      .attr("x", rem2px(0.1))
+      .attr("y", (d, i) => rem2px(0.7) + i * rem2px(0.7))
+      .text((d) => d)
+      .on("mouseover", showTooltip)
+      .on("mouseout", hideTooltip);
+
+  // Create Legend.
+  const categories = root.leaves()
+      .map((nodes) => nodes.data.category)
+      .filter((category, index, self) => self.indexOf(category) === index);
+
+  const legendElementsPerRow = Math.floor(legendWidth / legendSize.hSpacing);
+
+  const legendElement = legend
+      .append("g")
+      .attr("transform", "translate(60," + legendSize.offset + ")")
+      .selectAll("g")
+      .data(categories)
+      .enter().append("g")
+      .attr("transform", (d, i) => 'translate(' +
+            ((i % legendElementsPerRow) * legendSize.hSpacing)
+          + ',' +
+            ((Math.floor(i / legendElementsPerRow))
+             * legendSize.rectSize
+             + (legendSize.vSpacing * (Math.floor(i / legendElementsPerRow))))
+          + ')'
+      );
+
+  legendElement.append("rect")
+      .attr('width', legendSize.rectSize)
+      .attr('height', legendSize.rectSize)
+      .attr('class', 'legend-item')
+      .attr('fill', (d) => color(d));
+
+  legendElement.append("text")
+      .attr('x', legendSize.rectSize + legendSize.xOffset)
+      .attr('y', legendSize.rectSize + legendSize.yOffset)
+      .text((d) => d);
+};
+
+// Creates a Chart on start or on Menu Click.
+const createChart = (query) => {
+  // Set title.
+  title.innerHTML = FILES[query].title;
+
+  // Create Chart.
+  d3.json(FILES[query].url)
+      .then((data) => d3Process(data));
+};
+
+// Add listener for "smoother" menu-click.
+const links = document.getElementsByClassName('nav-link');
+Array.from(links).forEach((element) =>
+    element.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetDataUrl = new URL(e.target.href);
+      const newQuery = targetDataUrl.searchParams.get("data") || 'game';
+      svgChart.selectAll('*').remove();
+      legend.selectAll('*').remove();
+
+      createChart(newQuery);
+    })
+);
+
+// Get Parameter.
+const queryUrl = new URL(window.location);
+const dataQuery = queryUrl.searchParams.get("data") || 'game';
+
+createChart(dataQuery);
--- dist/d3tm.js
+++ dist/d3tm.js
@@ -7,111 +7,133 @@ var rem2px = function rem2px(rem) {
   return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 };
 
-var FILES = ['https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json', 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'];
-var container = document.getElementById('container'),
+var FILES = {
+  kickstarter: {
+    title: "Kickstarter Pledges",
+    url: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json"
+  },
+  movie: {
+    title: "Movie Sales",
+    url: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json"
+  },
+  game: {
+    title: "Video Game Sales",
+    url: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json"
+  }
+};
+var title = document.getElementById('title'),
     description = document.getElementById('description'),
-    margin = {
-  top: rem2px(1),
-  right: rem2px(1),
-  bottom: rem2px(4),
-  left: rem2px(1)
-},
-    padding = {
-  top: rem2px(4.5),
-  right: rem2px(1),
-  bottom: rem2px(1),
-  left: rem2px(1)
-},
-    width = container.offsetWidth - margin.left - margin.right - padding.left - padding.right,
-    height = container.offsetHeight - margin.top - margin.bottom - padding.top - padding.bottom;
-width = width < 960 ? 960 : width;
-var x = d3.scaleLinear().domain([2.6, 75.1]).rangeRound([height, width]),
-    color = d3.scaleThreshold().domain(d3.range(2.6, 75.1, (75.1 - 2.6) / 8)).range(d3.schemeBlues[9]),
-    path = d3.geoPath();
-var chart = d3.select('.chart');
-var svgChart = chart.append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + padding.bottom);
-var tooltip = chart.append('div').attr('id', 'tooltip').style('opacity', 0);
-var legend = svgChart.append("g").attr("class", "key").attr("id", "legend").attr("transform", "translate(0,40)");
-legend.selectAll("rect").data(color.range().map(function (d) {
-  d = color.invertExtent(d);
-  if (d[0] == null) d[0] = x.domain()[0];
-  if (d[1] == null) d[1] = x.domain()[1];
-  return d;
-})).enter().append("rect").attr("height", 8).attr("x", function (d) {
-  return x(d[0]);
-}).attr("width", function (d) {
-  return Math.abs(x(d[1]) - x(d[0]));
-}).attr("fill", function (d) {
-  return color(d[0]);
-});
-legend.append("text").attr("class", "caption").attr("x", x.range()[0]).attr("y", -6).attr("fill", "#000").attr("text-anchor", "start").attr("font-weight", "bold").text("Educational Attainment");
-legend.call(d3.axisBottom(x).tickSize(13).tickFormat(function (x) {
-  return Math.round(x) + '%';
-}).tickValues(color.domain())).select(".domain").remove(); // Create Chart promisified.
-
-Promise.all(FILES.map(function (url) {
-  return d3.json(url);
-})).then(function (data) {
-  return d3Process(data);
-});
+    legendSize = {
+  offset: rem2px(0.5),
+  xOffset: 3,
+  yOffset: -2,
+  rectSize: rem2px(0.8),
+  hSpacing: rem2px(10),
+  vSpacing: rem2px(0.5)
+};
+var svgChart = d3.select("#tree-map"),
+    width = +svgChart.attr("width"),
+    height = +svgChart.attr("height"),
+    body = d3.select('body'),
+    legend = d3.select("#legend"),
+    legendWidth = +legend.attr("width");
+var tooltip = body.append("div").attr("id", "tooltip").style("opacity", 0);
 
 var d3Process = function d3Process(data) {
-  // Fill US & Education Array from data.
-  var us = data[0].hasOwnProperty('type') ? data[0] : data[1];
-  var education = data[0].hasOwnProperty('type') ? data[1] : data[0]; // Add counties.
+  var color = d3.scaleOrdinal().range(d3.schemeCategory10.map(function (c) {
+    return d3.interpolateRgb(c, "#ccc")(0.1);
+  })),
+      format = d3.format(',d'),
+      treemap = d3.treemap().size([width, height]).paddingInner(1);
+  description.innerHTML = data.name;
 
-  svgChart.append("g").attr("class", "counties").selectAll("path").data(topojson.feature(us, us.objects.counties).features).enter().append("path").attr("class", "county").attr("data-fips", function (d) {
-    return d.id;
-  }).attr("data-education", function (d) {
-    var result = education.filter(function (obj) {
-      return obj.fips === d.id;
-    });
+  var showTooltip = function showTooltip(d) {
+    tooltip.style("opacity", .9).attr("data-value", d.data.value).style("left", d3.event.pageX + rem2px(1) + "px").style("top", d3.event.pageY + rem2px(1) + "px").html('Name: ' + d.data.name + '<br>' + 'Category: ' + d.data.category + '<br>' + 'Value: ' + d.data.value);
+  };
 
-    if (result[0]) {
-      return result[0].bachelorsOrHigher;
-    } // Could not find a matching fips-id in the data.
-
-
-    console.log("Couldn't find data for: ", d.id);
-    return 0;
-  }).attr("fill", function (d) {
-    var result = education.filter(function (obj) {
-      return obj.fips === d.id;
-    });
-
-    if (result[0]) {
-      return color(result[0].bachelorsOrHigher);
-    } // Could not find a matching color in the Data.
-
-
-    return color(0);
-  }).attr("d", path).on("mouseover", function (d) {
-    var result = education.filter(function (obj) {
-      return obj.fips === d.id;
-    });
-
-    if (result[0]) {
-      tooltip.style("opacity", .9);
-      tooltip.attr("data-education", result[0].bachelorsOrHigher);
-      tooltip.html(result[0].area_name + ", " + result[0].state + ": " + result[0].bachelorsOrHigher + "%"); // Calculate x and y positions for tooltip.
-
-      var mouseX = d3.event.pageX - svgChart.node().getBoundingClientRect().x + 10,
-          mouseY = d3.event.pageY - svgChart.node().getBoundingClientRect().y + 10,
-          y = mouseY + tooltip.node().offsetHeight + rem2px(1) > height ? mouseY - tooltip.node().offsetHeight / 2 + rem2px(1) : mouseY + tooltip.node().offsetHeight + rem2px(1),
-          _x = mouseX - tooltip.node().offsetWidth / 2 > width ? mouseX - tooltip.node().offsetWidth + rem2px(1) : mouseX - tooltip.node().offsetWidth / 2 + rem2px(1);
-
-      if (_x < 0) {
-        _x = mouseX;
-      }
-
-      tooltip.style("left", _x + "px").style("top", y + "px");
-    }
-  }).on("mouseout", function (d) {
+  var hideTooltip = function hideTooltip(d) {
     tooltip.style("opacity", 0);
-  }); // Add states.
+  }; // Create Root tree.
 
-  svgChart.append("path").datum(topojson.mesh(us, us.objects.states, function (a, b) {
-    return a !== b;
-  })).attr("class", "states").attr("d", path);
-};
+
+  var root = d3.hierarchy(data).eachBefore(function (d) {
+    d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name;
+  }).sum(function (d) {
+    return d.value;
+  }).sort(function (a, b) {
+    return b.value - a.value || b.height - a.height;
+  });
+  treemap(root); // Create cells.
+
+  var cell = svgChart.selectAll("g").data(root.leaves()).enter().append("g").attr("class", "group").attr("transform", function (d) {
+    return "translate(" + d.x0 + "," + d.y0 + ")";
+  }); // Create tiles.
+
+  cell.append("rect").attr("id", function (d) {
+    return d.data.id;
+  }).attr("class", "tile").attr("width", function (d) {
+    return d.x1 - d.x0;
+  }).attr("height", function (d) {
+    return d.y1 - d.y0;
+  }).attr("data-name", function (d) {
+    return d.data.name;
+  }).attr("data-category", function (d) {
+    return d.data.category;
+  }).attr("data-value", function (d) {
+    return d.data.value;
+  }).attr("fill", function (d) {
+    return color(d.data.category);
+  }).on("mouseover", showTooltip).on("mouseout", hideTooltip); // Add Tile Texts.
+
+  cell.append("text").attr('class', 'tile-text').selectAll("tspan").data(function (d) {
+    return d.data.name.split(/(?=[A-Z][^A-Z])/g);
+  }).enter().append("tspan").attr("x", rem2px(0.1)).attr("y", function (d, i) {
+    return rem2px(0.7) + i * rem2px(0.7);
+  }).text(function (d) {
+    return d;
+  }).on("mouseover", showTooltip).on("mouseout", hideTooltip); // Create Legend.
+
+  var categories = root.leaves().map(function (nodes) {
+    return nodes.data.category;
+  }).filter(function (category, index, self) {
+    return self.indexOf(category) === index;
+  });
+  var legendElementsPerRow = Math.floor(legendWidth / legendSize.hSpacing);
+  var legendElement = legend.append("g").attr("transform", "translate(60," + legendSize.offset + ")").selectAll("g").data(categories).enter().append("g").attr("transform", function (d, i) {
+    return 'translate(' + i % legendElementsPerRow * legendSize.hSpacing + ',' + (Math.floor(i / legendElementsPerRow) * legendSize.rectSize + legendSize.vSpacing * Math.floor(i / legendElementsPerRow)) + ')';
+  });
+  legendElement.append("rect").attr('width', legendSize.rectSize).attr('height', legendSize.rectSize).attr('class', 'legend-item').attr('fill', function (d) {
+    return color(d);
+  });
+  legendElement.append("text").attr('x', legendSize.rectSize + legendSize.xOffset).attr('y', legendSize.rectSize + legendSize.yOffset).text(function (d) {
+    return d;
+  });
+}; // Creates a Chart on start or on Menu Click.
+
+
+var createChart = function createChart(query) {
+  // Set title.
+  title.innerHTML = FILES[query].title; // Create Chart.
+
+  d3.json(FILES[query].url).then(function (data) {
+    return d3Process(data);
+  });
+}; // Add listener for "smoother" menu-click.
+
+
+var links = document.getElementsByClassName('nav-link');
+Array.from(links).forEach(function (element) {
+  return element.addEventListener('click', function (e) {
+    e.preventDefault();
+    var targetDataUrl = new URL(e.target.href);
+    var newQuery = targetDataUrl.searchParams.get("data") || 'game';
+    svgChart.selectAll('*').remove();
+    legend.selectAll('*').remove();
+    createChart(newQuery);
+  });
+}); // Get Parameter.
+
+var queryUrl = new URL(window.location);
+var dataQuery = queryUrl.searchParams.get("data") || 'game';
+createChart(dataQuery);
 //# sourceMappingURL=d3tm.js.map
